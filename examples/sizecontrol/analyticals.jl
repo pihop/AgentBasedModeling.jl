e46f16911913f4672aaa913be25aa76282c6fea0
Paul Piho <paul.piho@gmail.com>
2024-08-26 18:10:14 +0100

    change model, a bit of tidy

--- examples/sizecontrol/analyticals.jl
+++ examples/sizecontrol/analyticals.jl
@@ -7,26 +7,25 @@ using Integrals
 using Intervals
 using Interpolations
 
-α_ = 0.05
-kprod_ = 2.0 
-b_ = 2.0
+α_ = 1.0
+kprod_ = 10.0 
+b_ = 6.0
 μ_ = 1.0
 cv_ = 0.2 
 
-lowTriIter2(A::AbstractMatrix) = 
-  Iterators.flatten(Iterators.map(j->(j,i), i:size(A,1)) for i=1:size(A,2))
+span_ = (1e-4, 0.8)
 
 intparams = ( 
     abstol = 1e-4,
     reltol = 1e-4)
 
-γ(s, s0) = 1/(α_*s) * γdiv(μ_, cv_, s, s0)
+γ(s, s0) = γdiv(μ_, cv_, s, s0)
 γint(s, s0) = IntegralProblem((u,p) -> γ(u, s0), (s0, s))
-phi(s, s0) = γ(s, s0) * exp(-solve(γint(s, s0), QuadGKJL()).u)
+phi(s, s0) = γ(s, s0) * exp(-solve(γint(s, s0), QuadGKJL(); intparams...).u)
 
-ddist = Beta(7, 7)
+ddist = Beta(100)
 
-kernelf(s, s0) = IntegralProblem((u,p) -> 2*pdf(ddist, u) * phi(s/u, s0), (0.0001, 1.0))
+kernelf(s, s0) = IntegralProblem((u,p) -> 2*pdf(ddist, u) * phi(s/u, s0), span_)
 ker(s, s0) = solve(kernelf(s, s0), QuadGKJL(); intparams...).u
 
 function trapz(fx, xstep)
@@ -41,10 +40,10 @@ end
 function volterra(ker, span, n)
     a, b = span
     h = (b-a)/n
-    x = range(a, stop=b, step=h)
+    x = range(a, stop=b, length=n)
     
     Xi = Float64[]
-    Ai = zeros(length(x)-1, length(x)-1)
+    Ai = zeros(length(x), length(x))
     
     for i in 1:n
         for j in range(1, n, step=1)
@@ -57,9 +56,6 @@ function volterra(ker, span, n)
     return Ai
 end
 
-nbins=100
-
-span_ = (0.0001, 0.6)
 N = 50
 sstep_ = (span_[2]-span_[1])/N
 srange_ = collect(range(span_[1], stop=span_[2], length=N))
@@ -70,31 +66,34 @@ psiA_tree = Float64.(eigvecs(A)[:,end]) ./ srange_
 sn_tree = trapz(psiA_tree, sstep_)
 
 psi = linear_interpolation(
-    range(span_[1], stop=span_[2], length=N), Float64.(eigvecs(A)[:,end]) .* 1/sn)
+    range(span_[1], stop=span_[2], length=N), Float64.(eigvecs(A)[:,end]) .* 1/sn, extrapolation_bc=Line())
 psi_tree_ = linear_interpolation(
-    range(span_[1], stop=span_[2], length=N), psiA_tree .* 1/sn_tree)
-# Compute protein distribution for different birth sizes s0.
+    range(span_[1], stop=span_[2], length=N), psiA_tree .* 1/sn_tree, extrapolation_bc=Line())
 
+# Compute protein distribution for different birth sizes s0.
 function rho(s_, s0_, s0)
-    (1/psi(s0))*pdf(ddist, s0/s_)*phi(s_, s0_)*psi(s0_)
+    (1/psi(s0))*(s0/s_)*pdf(ddist, s0/s_)*phi(s_, s0_)*psi(s0_)
 end
 
+sintspan_ = [span_[1], 1.8]
 # Birth protein counts... Use concentration homeostasis. 
-dist(kprod, b, α, s) = NegativeBinomial(kprod/(α*s), 1/(2+b*s)) 
+dist(kprod, b, α, s) = NegativeBinomial(kprod/(α), 1/(1+b*s)) 
 Pi(x, s) = pdf(dist(kprod_, b_, α_, s), x)
 B(x, x_, θ) = pdf(Binomial(x_, θ), x)
 
 # Integrate
-iprob_s0(x, x_, s_, s0) = IntegralProblem((u, p) -> B(x, x_, s0/s_)*rho(s_, u, s0)*Pi(x_, s_), span_)
+rhoint(x, x_, s_, s0) = IntegralProblem((u, p) ->  rho(s_, u, s0), sintspan_)
+Pi0int(x, x_, s0) = IntegralProblem(
+    (u,p) -> B(x, x_, s0/u)*solve(rhoint(x, x_, u, s0), QuadGKJL(); intparams...).u *Pi(x_, u), (s0, sintspan_[end]))
+Pi0(x, x_, s0)  = solve(Pi0int(x, x_, s0), QuadGKJL(); intparams...).u
 
-idist(x, x_, s0) = solve(
-    IntegralProblem((u, p) -> solve(iprob_s0(x, x_, u, s0), QuadGKJL(); intparams...).u, (s0, span_[end])), QuadGKJL(); intparams...).u
-
-xs_ = floor.(collect(range(0, stop=200, length=20)))
+xs_ = collect(range(0, stop=150, step=5))
 xstep_ = xs_[2] - xs_[1]
-array_dists(x, s0) = trapz(map(x_ -> idist(x, x_, s0), xs_), xstep_)
-ss_ = range(span_[1], stop=span_[2], length=20)
+array_dists(x, s0) = map(x_ -> Pi0(x, x_, s0), xs_)
+ss_ = range(sintspan_[1], stop=sintspan_[2], length=10)
 
-mat = [array_dists(x, s)*psi_tree_(s) for x in xs_, s in ss_]
-mat_s = trapz.(eachrow(mat), Ref(sstep_))
+mat = [array_dists(x, s) for x in xs_, s in ss_]
+mat_ = hcat([row .* psi_tree_.(ss_) for row in eachrow(mat)]...)'
+mat_ = trapz.(mat_, Ref(xstep_))
+mat_s = trapz.(eachrow(mat_), Ref(sstep_))
 mat_sx = trapz(mat_s, xstep_)
--- examples/sizecontrol/main.jl
+++ examples/sizecontrol/main.jl
@@ -17,15 +17,17 @@ htransp = 0.05
 stairstransp = 0.9
 barstransp = 0.3
 
-@variables t 
+@independent_variables t 
 @species C(t) P(t) τ(t) s(t)
-@parameters Cτ Cs Cs0 CP Cθ s0 α θ[1:4] kprod K b μ cv L
+@parameters Cτ Cs Cs0 CP Cθ s0 α θ[1:4] kprod K b μ cv L Nmax r
 D = Differential(t)
 
 @register_symbolic Distributions.Geometric(a)
-m = rand(Distributions.Geometric(1/(2 + b*s))) + 1
+
+m = rand(Distributions.Geometric(1/(1 + b*s))) 
 
 disc_dynamics = @reaction_network begin
+    @species s(t)
     kprod, 0 --> $m*P 
 end
 
@@ -35,34 +37,31 @@ Cell = AgentDynamics((cont_dynamics, disc_dynamics), (s0, ))
 gammahaz(μ,cv,x) = exp(logpdf(Gamma(1/cv, μ*cv), x) - logccdf(Gamma(1/μ, μ*cv), x))
 
 # Adder rule for cell division.
-γdiv(μ, cv, s, s0) = gammahaz(μ, cv, s - s0)
-@register_symbolic γdiv(μ, cv, s, s0)
+γdiv(μ, cv, s, s0, α) = α * s * gammahaz(μ, cv, s - s0)
+@register_symbolic γdiv(μ, cv, s, s0, α)
 
 function γdivbound(μ, cv, τ, s0, L, α)
-    gammahaz(μ, cv, s0*exp(α*(τ+L)) - s0)
+   s0*exp(α*(τ+L)) * α * gammahaz(μ, cv, s0*exp(α*(τ+L)) - s0)
 end
 @register_symbolic γdivbound(μ, cv, τ, s0, L, α)
 
 function sample_beta()
-    rand(Beta(7, 7))
-end
-
-function binomial_part(x, p)
-    x_ = floor(x)
-    x1 = rand(Binomial(x_, p))
-    x2 = x_ - x1
-    return Float64(x1), Float64(x2)
+    rand(Beta(100))
 end
 
 function partition_cell(x)
     p = sample_beta()
-    return [p, 1-p, binomial_part(x,p)...]
+    x_ = floor(x)
+    x1 = rand(Binomial(x_, p))
+    x2 = x_ - x1
+    return [p, 1-p, x1, x2]
 end
+
 @register_symbolic binomial_part(a)
 @register_symbolic partition_cell(a)
 
 division = @interaction begin
-    @channel γdiv($μ, $cv, $Cs, $Cs0), $C --> 2*$C
+    @channel γdiv($μ, $cv, $Cs, $Cs0, $α), $C --> 2*$C
     @sampler ExtrandeMethod($γdivbound($μ, $cv, $Cτ, $Cs0, $L, $α), $L)
     @variable $θ = $partition_cell($CP)
     @transition (
@@ -77,23 +76,21 @@ end
 
 # Put them together.
 population_model = PopulationModelDef([division, ], Dict(C => Cell,))
-
 # Initial population state. Starting with a single cell at age 0.
-init_pop = repeat([C,], 1) .=> repeat([(P => 60.0, τ => 0.0, s => 0.20, s0 => 0.20),], 1)
-tspan_pop = (0.0, 5000.0)
+init_pop = repeat([C,], 1) .=> repeat([(P => 30.0, τ => 0.0, s => 0.50, s0 => 0.50),], 1)
+tspan_pop = (0.0, 20.0)
 # Simulator timestep.
-Δt = 10.0
+Δt = 1.0
 
 simulation_params = SimulationParameters(
-    [α => 0.05, kprod => 2.0, b => 2.0, μ => 1.0, cv => 0.2, L => 1.0], 
+    [α => 1.0, kprod => 10.0, b => 6.0, μ => 1.0, cv => 0.2, L => 0.1], 
     tspan_pop, 
     Δt, 
     Rodas5(); 
     snapshot=[PopulationSnapshot(C),], 
-    maxpop=50000)
+    maxpop=20000)
 
-#res_pop = simulate(population_model, init_pop, simulation_params; remember_all_agents=true)
-res = res_pop
+res = simulate(population_model, init_pop, simulation_params; remember_all_agents=true)
 
 # Extract the lineages from simulation results.
 saveΔt = 1.0
@@ -103,33 +100,17 @@ for agent in values(res.final_pop[C])
     lin = Dict()
     lineage = AgentBasedModeling.lineage(res, agent)[1:end-1]
     for a in lineage
-        tstart = a.btime
-        tend = a.dtime
-        trange = tstart:saveΔt:tend
-
+        trange = a.btime:saveΔt:a.dtime
         adict = Dict(
-            :fluor => a.simulation(trange; idxs =2).u,
-            :size => a.simulation(trange; idxs = 1).u,
-            :age => a.simulation(trange; idxs = 3).u,
-            :addedlength => a.simulation(trange; idxs = 1).u[end] - a.simulation(trange; idxs = 1).u[1])
+            :fluor => a.simulation(trange; idxs = P).u,
+            :size => a.simulation(trange; idxs = s).u)
         lin[a.uid] = adict
         agents[a.uid] = adict
     end
     push!(lineages, lin)
 end
 
-snapshot = []
-for a in values(res.agents[C])
-    isnothing(a.dtime) && continue 
-    isnothing(a.btime) && continue 
-    if a.dtime > 215 && a.btime < 215
-        push!(snapshot, a)
-    end
-end
-snapshot_bprotein = [a.simulation[1][1] for a in snapshot] 
-
 #Plot results
-
 pt_cm = 2.83465
 fig = Figure(size=(170, 50) .* pt_cm; fontsize=8, pt_per_unit=1)
 ax_protein = Axis(fig[1,1]; xlabel="Birth protein distribution", ylabel="Probability density")
@@ -141,36 +122,34 @@ hidespines!(ax_protein, :t, :r)
 hidedecorations!(ax_size, ticklabels = false, ticks = false, label = false)
 hidespines!(ax_size, :t, :r) 
 
-sizehist = normalize(fit(Histogram, getfield.(res.prods[:Cs], :value), range(0., stop=0.6, length=50)); mode=:pdf)
+sizehist = normalize(fit(Histogram, getfield.(res.prods[:Cs], :value), range(0.2, stop=0.8, length=50)); mode=:pdf)
 stairs!(ax_size, collect(midpoints(sizehist.edges[1])), sizehist.weights; color=(colors[1], stairstransp), step=:center, label="Agent-based simulation")
 barplot!(ax_size, collect(midpoints(sizehist.edges[1])), sizehist.weights; 
     color=(colors[1], barstransp), strokecolor=(colors[1], transp), strokewidth=0.0, gap=0.0, dodge_gap=0.0)
 
-proteinsnap = normalize(fit(Histogram, snapshot_bprotein, range(0, stop=160, length=40)); mode=:pdf)
+proteinsnap = normalize(fit(Histogram, [Dict(a.init_trait)[P] for a in values(res.final_pop[C])], range(0, stop=100, length=100)); mode=:pdf)
 stairs!(ax_protein, collect(midpoints(proteinsnap.edges[1])), proteinsnap.weights; color=(colors[1], stairstransp), step=:center, label="Agent-based simulation")
 barplot!(ax_protein, collect(midpoints(proteinsnap.edges[1])), proteinsnap.weights; 
     color=(colors[1], barstransp), strokecolor=(colors[1], transp), strokewidth=0.0, gap=0.0, dodge_gap=0.0)
 
 # Comparison with analytical computations.
 include("analyticals.jl")
-N_ = 50
 lines!(ax_size, range(span_[1], stop=span_[2], length=N_), psi_tree_.(range(span_[1], stop=span_[2], length=N_)); color=colors[10], label="Analytical solution")
 lines!(ax_protein, xs_, mat_s ./ mat_sx; color=colors[10], label="Analytical solution")
 
 function plot_lineage(axsize, axprotein, agent; kwargs...)
     lin = AgentBasedModeling.lineage(res, agent)
-    for l in lin[end-6:end]
+    for l in lin[end-8:end]
         sim = l.simulation
         tstart = l.btime
         if !isnothing(l.dtime)
             tend = l.dtime
-            sim_ = sim(tstart:0.1:tend)
-            lines!(axsize, sim_.t, getindex.(sim_.u, 2); kwargs...)
-            lines!(axprotein, sim_.t, getindex.(sim_.u, 1); kwargs...)
+            lines!(axsize, sim(tstart:0.1:tend).t, sim(tstart:0.1:tend; idxs=s).u; kwargs...)
+            lines!(axprotein, sim(tstart:0.1:tend).t, sim(tstart:0.1:tend; idxs=P).u; kwargs...)
         else
-            sim_ = sim(tstart:0.1:sim.t[end])
-            lines!(axsize, sim_.t, getindex.(sim_.u, 2); kwargs...)
-            lines!(axprotein , sim_.t, getindex.(sim_.u, 1); kwargs...)
+            tend = sim.t[end]
+            lines!(axsize, sim(tstart:0.1:tend).t, sim(tstart:0.1:tend; idxs=s).u; kwargs...)
+            lines!(axprotein , sim(tstart:0.1:tend).t, sim(tstart:0.1:tend; idxs=P).u; kwargs...)
         end
     end
 end
@@ -202,12 +181,12 @@ ax_traj_size = Axis(fig[1,2]; xlabel="Time", ylabel="Size", title = "Lineage tra
     valign=1.0,)
 
 i = 1
-for agent in last.(collect(Iterators.flatten(values(res.final_pop))))[end-50:end-1]
+for agent in rand(last.(collect(Iterators.flatten(values(res.final_pop)))), 50)
     plot_lineage(ax_traj_size, ax_traj_protein, agent; color = (colors[i], htransp), linewidth=0.8)
     global i = mod(i + 1, length(colors)) + 1
 end
 
-agent = last.(collect(Iterators.flatten(values(res.final_pop))))[end]
+agent = rand(last.(collect(Iterators.flatten(values(res.final_pop)))))
 plot_lineage(ax_traj_size, ax_traj_protein, agent; color = colors[1], linewidth=1.2)
 hidedecorations!(ax_traj_size, ticklabels = false, ticks = false, label = false)
 hidespines!(ax_traj_size, :t, :r) 
@@ -215,10 +194,12 @@ hidespines!(ax_traj_size, :t, :r)
 hidedecorations!(ax_traj_protein, ticklabels = false, ticks = false, label = false)
 hidespines!(ax_traj_protein, :t, :r) 
 
-xlims!(ax_traj_protein, low=150)
-xlims!(ax_traj_size, low=150)
-ylims!(ax_traj_protein, high=200)
-ylims!(ax_traj_size, high=0.7)
+xlims!(ax_size, (0.2, 1.0))
+xlims!(ax_protein, (0, 100))
+xlims!(ax_traj_protein, low=6)
+xlims!(ax_traj_size, low=6)
+ylims!(ax_traj_protein, high=90)
+ylims!(ax_traj_size, high=1.2)
 
 mkpath("$(plotsdir())/size_control/")
 save("$(plotsdir())/size_control/sizecontrol_traj.pdf", fig; pt_per_unit = 1.0)
