edb6b4929947d72cb5cda46707098f377639aef0
Paul Piho <paul.pihp@gmail.com>
2025-02-26 10:54:10 +0000

    deduplicate knots

--- src/results.jl
+++ src/results.jl
@@ -107,6 +107,7 @@ function build_snapshot_solution(snapshot; names)
     us = [[s...] for s in snaps] 
     T = eltype(eltype(us))
     N = length((size(us[1])..., length(us)))
+    Interpolations.deduplicate_knots!(t)
     interp = Interpolations.linear_interpolation(t, us)  
     retcode = ReturnCode.Success
     return SnapshotSolution{T,N}(names, us, t, interp, retcode)
