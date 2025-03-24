d82247b37804e61bc66542a6e638c96c38203b9d
Paul Piho <paul.pihp@gmail.com>
2025-03-24 09:38:16 +0000

    remake troubles is scimlbase. u0 as vector of values fails quietly. use symbolic map instead

--- src/simulation.jl
+++ src/simulation.jl
@@ -117,7 +117,8 @@ end
 function simulate_internal(problem, agent, init, tspan, params; model, kwargs...)
     unk = unknowns(model.traitdefs[agent.sym].dynamics)
     u0map = ModelingToolkit.varmap_to_vars(init, unk)
-    prob = remake(problem, u0=u0map, tspan=tspan)
+
+    prob = remake(problem; u0 = unk .=> u0map, tspan=tspan)
 
     if (problem isa JumpProblem && problem.prob isa DiscreteProblem)
         return solve(prob, params.jumpsolver; kwargs...), params.jumpsolver 
